d5a912fc6cb9e2dbff11222e33284b64e152de9a
Artem <artemvar@ucm.es>
2026-02-04 22:27:15 +0100

    param to turn Open Game Data integration on/off

--- src/lib/ogdTracker.js
+++ src/lib/ogdTracker.js
@@ -2,5 +2,6 @@ import {OGDLogger} from 'opengamedata-js-log'
 let appName = 'CONECTADO';
 let version = '1.0.0'; // checkme version should be detected automatically. also what about log version (js-tracker version?)
 var ogdTracker = new OGDLogger(appName, version)
+ogdTracker.enabled = true; // set to false to disable
 
 export default ogdTracker;
