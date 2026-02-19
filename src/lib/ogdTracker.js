d9c89b65cbef17daafab15cb90290904eeefb112
Artem <artemvar@ucm.es>
2026-02-19 13:42:58 +0100

    ogd - avoid double initialization. actor id generation. utf-8 encoding fixed.

--- src/lib/ogdTracker.js
+++ src/lib/ogdTracker.js
@@ -2,6 +2,7 @@ import {OGDLogger} from 'opengamedata-js-log'
 let appName = 'CONECTADO';
 let version = '1.0.0'; // checkme version should be detected automatically. also what about log version (js-tracker version?)
 var ogdTracker = new OGDLogger(appName, version)
-ogdTracker.enabled = true; // set to false to disable
+
+ogdTracker.initialized = false; // evitar volver a sobreescribir el send
 
 export default ogdTracker;
